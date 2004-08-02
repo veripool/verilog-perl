3a5df3ddbdb36722841f2531d30db78877d0fb9d
Wilson Snyder <wsnyder@wsnyder.org>
2004-08-02 20:04:08 +0000

    Remove perl path from non-exec test_utils [P4 Change 45639]
    git-svn-id: file://localhost/svn/Verilog-Perl/trunk/Verilog-Perl@439 c99bdd1b-02e9-0310-b855-c9df8d5cb90a

---
+++ t/test_utils.pl
@@ -0,0 +1,52 @@
+# $Revision: 1.13 $$Date: 2004/08/02 20:03:45 $$Author: wsnyder $
+# DESCRIPTION: Perl ExtUtils: Common routines required by package tests
+#
+# Copyright 2000-2004 by Wilson Snyder.  This program is free software;
+# you can redistribute it and/or modify it under the terms of either the GNU
+# General Public License or the Perl Artistic License.
+
+use IO::File;
+use vars qw($PERL);
+
+$PERL = "$^X -Iblib/arch -Iblib/lib -IPreproc/blib/arch -IPreproc/blib/lib";
+
+mkdir 'test_dir',0777;
+
+if (!$ENV{HARNESS_ACTIVE}) {
+    use lib '.';
+    use lib '..';
+    use lib "blib/lib";
+    use lib "blib/arch";
+    use lib "Preproc/blib/lib";
+    use lib "Preproc/blib/arch";
+}
+
+sub run_system {
+    # Run a system command, check errors
+    my $command = shift;
+    print "\t$command\n";
+    system "$command";
+    my $status = $?;
+    ($status == 0) or die "%Error: Command Failed $command, $status, stopped";
+}
+
+sub files_identical {
+    my $fn1 = shift;
+    my $fn2 = shift;
+    my $f1 = IO::File->new ($fn1) or die "%Error: $! $fn1,";
+    my $f2 = IO::File->new ($fn2) or die "%Error: $! $fn2,";
+    my @l1 = $f1->getlines();
+    my @l2 = $f2->getlines();
+    my $nl = $#l1;  $nl = $#l2 if ($#l2 > $nl);
+    for (my $l=0; $l<=$nl; $l++) {
+	if (($l1[$l]||"") ne ($l2[$l]||"")) {
+	    warn ("%Warning: Line ".($l+1)." mismatches; $fn1 != $fn2\n"
+		  ."F1: ".($l1[$l]||"*EOF*\n")
+		  ."F2: ".($l2[$l]||"*EOF*\n"));
+	    return 0;
+	}
+    }
+    return 1;
+}
+
+1;
