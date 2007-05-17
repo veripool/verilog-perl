19aae5fe248786f4499c76896ddcdef88f73ad0a
Wilson Snyder <wsnyder@wsnyder.org>
2007-05-17 16:47:10 +0000

    Verilog::Preproc: Ignore quotes inside protected/endprotected blocks
    git-svn-id: file://localhost/svn/Verilog-Perl/trunk/Verilog-Perl@38862 c99bdd1b-02e9-0310-b855-c9df8d5cb90a

--- verilog/inc1.v
+++ verilog/inc1.v
@@ -58,3 +58,11 @@ $display(`msg(left side, right side))
 `ifndef EMPTY_TRUE
   `error "Empty is still true"
 `endif
+
+// Quotes are legal in protected blocks.  Grr.
+module prot();
+`protected
+    I!#r#e6<_Q{{E2+]I3<[3s)1@D|'E''i!O?]jD>Jo_![Cl)
+    #nj1]p,3^1~,="E@QZB\T)eU\pC#C|7=\$J$##A[@-@{Qk]
+`endprotected
+endmodule
