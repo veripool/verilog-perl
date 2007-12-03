4d91f93286c97fb44f31f973d6f65226de4a3010
Wilson Snyder <wsnyder@wsnyder.org>
2007-12-03 17:41:16 +0000

    Verilog-Perl: new test
    git-svn-id: file://localhost/svn/Verilog-Perl/trunk/Verilog-Perl@48164 c99bdd1b-02e9-0310-b855-c9df8d5cb90a

--- verilog/parser_bugs.v
+++ verilog/parser_bugs.v
@@ -206,4 +206,6 @@ module foreqn (in);
    reg [1:0]  c;
    always for ({a,c[0]} = in; a < 1'b1; {b,c[1]} = in) begin
    end
+   always for ({a,c[in]} = 0; a < 1'b1; {b,c[in]} = 2'b10) begin
+   end
 endmodule
