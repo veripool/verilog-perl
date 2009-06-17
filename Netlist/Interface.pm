6cc93d34f8666a8bdac27775ba1e3a55b88ad0fb
Wilson Snyder <wsnyder@wsnyder.org>
2009-06-17 07:40:53 -0400

    Version bump

--- Netlist/Interface.pm
+++ Netlist/Interface.pm
@@ -14,7 +14,7 @@ use strict;
 @ISA = qw(Verilog::Netlist::Interface::Struct
 	Verilog::Netlist::Subclass);
 
-$VERSION = '3.210';
+$VERSION = '3.211';
 
 structs('new',
 	'Verilog::Netlist::Interface::Struct'
--- Parser/SigParser.pm
+++ Parser/SigParser.pm
@@ -14,7 +14,7 @@ use base qw(Verilog::Parser);
 ######################################################################
 #### Configuration Section
 
-$VERSION = '3.210';
+$VERSION = '3.211';
 
 our @_Callback_Names = qw(
   attribute
