0b9d06610f1e8a7604db1e78a1cb5e0d5d71e097
Wilson Snyder <wsnyder@wsnyder.org>
2009-10-22 17:18:28 -0400

    Fix erroring on strings with backslashed newlines, bug168.

--- verilog/parser_bugs.v
+++ verilog/parser_bugs.v
@@ -360,3 +360,10 @@ endmodule
 module bugmodportcomma (,a,);
    input a;
 endmodule
+
+module bug168;
+   initial $display("\nWarning!  This is a\
+              string with a line\
+              continuation\
+              at time %0d PS", $time);
+endmodule
