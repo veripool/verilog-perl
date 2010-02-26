d673a4e90ccb1afa29403dc5d5403aedc81808da
Wilson Snyder <wsnyder@wsnyder.org>
2010-02-26 17:19:24 -0500

    Fix Mac OS-X build.

--- Preproc/VPreprocLex.h
+++ Preproc/VPreprocLex.h
@@ -86,10 +86,17 @@ typedef struct yy_buffer_state *YY_BUFFER_STATE;
 # define YY_BUF_SIZE 16384
 #endif
 
+// Older flex'es don't have this defined, so make everyone happy
+#ifndef YY_TYPEDEF_YY_SIZE_T
+#define YY_TYPEDEF_YY_SIZE_T
+typedef size_t yy_size_t;
+#endif
+
 extern int yylex();
 extern void yyrestart(FILE*);
 extern char* yytext;
-extern int yyleng;
+extern size_t yyleng;
+
 YY_BUFFER_STATE yy_create_buffer ( FILE *file, int size );
 void yy_switch_to_buffer( YY_BUFFER_STATE new_buffer );
 void yy_delete_buffer( YY_BUFFER_STATE b );
