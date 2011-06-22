9fdf40505d4874e0659033597e60c29ae9b8c943
Wilson Snyder <wsnyder@wsnyder.org>
2011-06-22 16:02:17 -0400

    New test case

--- verilog/parser_sv.v
+++ verilog/parser_sv.v
@@ -355,3 +355,13 @@ module not_a_bug315;
    input imp_test_pkg::byte_t i;
    input supply_net_t bug316;
 endmodule
+
+module bins_bracket;
+   parameter N = 2;
+   covergroup cg_debitor @(posedge eclk);
+      count: coverpoint count iff (erst_n) {
+         // 'std' overrides std:: package, which confuses VP
+	 //bins  std[] = { [0:N] };
+      }
+   endgroup
+endmodule
