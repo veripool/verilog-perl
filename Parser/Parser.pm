578faf75b5926dbf0c1f63b379609396786976d1
Wilson Snyder <wsnyder@wsnyder.org>
2016-02-02 18:36:29 -0500

    Version bump

--- Parser/Parser.pm
+++ Parser/Parser.pm
@@ -14,7 +14,7 @@ use base qw(DynaLoader);
 use strict;
 use vars qw($VERSION $Debug);
 
-$VERSION = '3.417';
+$VERSION = '3.418';
 
 #$Debug sets the default value for debug.  You're better off with the object method though.
 
@@ -525,7 +525,7 @@ Verilog-Perl is part of the L<http://www.veripool.org/> free Verilog EDA
 software tool suite.  The latest version is available from CPAN and from
 L<http://www.veripool.org/verilog-perl>.
 
-Copyright 2000-2015 by Wilson Snyder.  This package is free software; you
+Copyright 2000-2016 by Wilson Snyder.  This package is free software; you
 can redistribute it and/or modify it under the terms of either the GNU
 Lesser General Public License Version 3 or the Perl Artistic License Version 2.0.
 
--- Parser/SigParser.pm
+++ Parser/SigParser.pm
@@ -14,7 +14,7 @@ use base qw(Verilog::Parser);
 ######################################################################
 #### Configuration Section
 
-$VERSION = '3.417';
+$VERSION = '3.418';
 
 our @_Callback_Names = qw(
   attribute
@@ -518,7 +518,7 @@ Verilog-Perl is part of the L<http://www.veripool.org/> free Verilog EDA
 software tool suite.  The latest version is available from CPAN and from
 L<http://www.veripool.org/verilog-perl>.
 
-Copyright 2000-2015 by Wilson Snyder.  This package is free software; you
+Copyright 2000-2016 by Wilson Snyder.  This package is free software; you
 can redistribute it and/or modify it under the terms of either the GNU
 Lesser General Public License Version 3 or the Perl Artistic License Version 2.0.
 
