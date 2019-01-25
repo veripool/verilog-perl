37addd2b80c447007dc76334350573c916b6af73
Wilson Snyder <wsnyder@wsnyder.org>
2019-01-24 19:00:05 -0500

    Version bump

--- Netlist/Logger.pm
+++ Netlist/Logger.pm
@@ -7,7 +7,7 @@ require Exporter;
 use vars qw($VERSION);
 use strict;
 
-$VERSION = '3.456';
+$VERSION = '3.458';
 
 # We don't use Verilog::Netlist::Subclass, as this is called from it!
 
@@ -167,7 +167,7 @@ Verilog-Perl is part of the L<http://www.veripool.org/> free Verilog EDA
 software tool suite.  The latest version is available from CPAN and from
 L<http://www.veripool.org/verilog-perl>.
 
-Copyright 2000-2018 by Wilson Snyder.  This package is free software; you
+Copyright 2000-2019 by Wilson Snyder.  This package is free software; you
 can redistribute it and/or modify it under the terms of either the GNU
 Lesser General Public License Version 3 or the Perl Artistic License Version 2.0.
 
--- Std.pm
+++ Std.pm
@@ -14,7 +14,7 @@ use vars qw($VERSION);
 ######################################################################
 #### Configuration Section
 
-$VERSION = '3.456';
+$VERSION = '3.458';
 
 #######################################################################
 # ACCESSORS
@@ -122,7 +122,7 @@ Verilog-Perl is part of the L<http://www.veripool.org/> free Verilog EDA
 software tool suite.  The latest version is available from CPAN and from
 L<http://www.veripool.org/verilog-perl>.
 
-Copyright 2009-2018 by Wilson Snyder.  This package is free software; you
+Copyright 2009-2019 by Wilson Snyder.  This package is free software; you
 can redistribute it and/or modify it under the terms of either the GNU
 Lesser General Public License Version 3 or the Perl Artistic License
 Version 2.0.
